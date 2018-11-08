a4e584246b3278c52da28bed7dfa317b32710d6e
nholschuh <nick.holschuh@gmail.com>
2018-11-08 10:43:02 -0800

    Update plot_ATL03_ATL06.m

--- matlab/NDH_plotter/plot_ATL03_ATL06.m
+++ matlab/NDH_plotter/plot_ATL03_ATL06.m
@@ -672,7 +672,7 @@ function h=plot_segs(x, h, slope, W, linespec)
 eval_string = ['h=plot([x(:)-W/2 x(:)+W/2]'', [h(:)-slope(:)*W/2 h(:)+slope(:)*W/2]'',', linespec,');'];
 eval(eval_string);
 end
-end
+
 
 
 function outval = strcmp_ndh(stringlist,comparator);
